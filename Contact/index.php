3d3a23bbe82ae9c78d33c0829f77373c883ce7fb
Thisal Chamodya <liyanaa-ps20022@stu.kln.ac.lk>
2025-05-02 20:11:11 +0530

    Contact page Added

---
+++ Contact/index.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+	<title>KW Industries - About Us</title>
+	<meta charset="utf-8">
+	<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<![endif]-->
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" href="../css/animations.css">
+	<link rel="stylesheet" href="../css/font-awesome5.css">
+	<link rel="stylesheet" href="../css/fontello.css">
+	<link rel="stylesheet" href="../css/main.css" class="color-switcher-link">
+	<link rel="stylesheet" href="../css/shop.css" class="color-switcher-link">
+	<script src="../js/vendor/modernizr-2.6.2.min.js"></script>
+
+	<!--[if lt IE 9]>
+		<script src="js/vendor/html5shiv.min.js"></script>
+		<script src="js/vendor/respond.min.js"></script>
+		<script src="js/vendor/jquery-1.12.4.min.js"></script>
+	<![endif]-->
+
+</head>
+
+<body>
+	<!--[if lt IE 9]>
+		<div class="bg-danger text-center">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" class="color-main">upgrade your browser</a> to improve your experience.</div>
+	<![endif]-->
+
+	<div class="preloader">
+		<div class="preloader_image"></div>
+	</div>
+
+    <div id="header"></div>
+    <section class="ls s-py-0 c-gutter-0 container-px-0 text-center text-sm-left">
+				<div class="container-fluid" id="faq">
+					<div class="row">
+						<div class="col-12">
+							<h6 class="special-heading sub-title text-center"><span>KW INDUSTRIES</span></h6>
+							<h2 class="special-heading text-center"> KW Industries FAQ</h2>
+							<div class="divider-50 hidden-below-md"></div>
+							<div class="divider-30 hidden-above-md"></div>
+						</div>
+					</div>
+					<div class="row ">
+						<div class="col-lg-6 py-xl-150 py-lg-130 py-md-90 py-60 ls ms">
+							<div class="content-center">
+								<div id="accordion01" role="tablist">
+									<h6 class="title-accordion">What is medical marijuana?</h6>
+									<div class="card">
+										<div class="card-header" role="tab" id="collapse01_header">
+											<h5>
+												<a class="collapsed" data-toggle="collapse" href="#collapse01" aria-expanded="true" aria-controls="collapse01">
+													What does Your Company Consulting do?
+												</a>
+											</h5>
+										</div>
+
+										<div id="collapse01" class="collapse" role="tabpanel" aria-labelledby="collapse01_header" data-parent="#accordion01">
+											<div class="card-body">
+												<p>We help entrepreneurs get ready to raise capital. This usually consists of some or all of the following services:</p>
+											</div>
+										</div>
+									</div>
+
+									<div class="card">
+										<div class="card-header" role="tab" id="collapse02_header">
+											<h5>
+												<a class="collapsed" data-toggle="collapse" href="#collapse02" aria-expanded="false" aria-controls="collapse02">
+													What industries do you specialize in?
+												</a>
+											</h5>
+										</div>
+										<div id="collapse02" class="collapse" role="tabpanel" aria-labelledby="collapse02_header" data-parent="#accordion01">
+											<div class="card-body">
+												Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+											</div>
+										</div>
+									</div>
+
+									<div class="card">
+										<div class="card-header" role="tab" id="collapse04_header">
+											<h5>
+												<a class="collapsed" data-toggle="collapse" href="#collapse04" aria-expanded="false" aria-controls="collapse04">
+													Can you guarantee that our plan will raise capital?
+												</a>
+											</h5>
+										</div>
+										<div id="collapse04" class="collapse" role="tabpanel" aria-labelledby="collapse04_header" data-parent="#accordion01">
+											<div class="card-body">
+												Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable sunt aliqua put a bird on it squid single-origin coffee shoreditch et.
+											</div>
+										</div>
+									</div>
+
+									<div class="card">
+										<div class="card-header" role="tab" id="collapse05_header">
+											<h5>
+												<a class="collapsed" data-toggle="collapse" href="#collapse05" aria-expanded="false" aria-controls="collapse05">
+													What industries do you specialize in?
+												</a>
+											</h5>
+										</div>
+										<div id="collapse05" class="collapse" role="tabpanel" aria-labelledby="collapse05_header" data-parent="#accordion01">
+											<div class="card-body">
+												Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+											</div>
+										</div>
+									</div>
+
+								</div>
+
+								<div id="accordion02" role="tablist">
+									<h6 class="title-accordion">What is medical marijuana used for?</h6>
+									<div class="card">
+										<div class="card-header" role="tab" id="collapse10_header">
+											<h5>
+												<a class="collapsed" data-toggle="collapse" href="#collapse10" aria-expanded="true" aria-controls="collapse10">
+													What does Your Company Consulting do?
+												</a>
+											</h5>
+										</div>
+
+										<div id="collapse10" class="collapse" role="tabpanel" aria-labelledby="collapse10_header" data-parent="#accordion02">
+											<div class="card-body">
+
+												<p>We help entrepreneurs get ready to raise capital. This usually consists of some or all of the following services:</p>
+
+
+											</div>
+										</div>
+									</div>
+
+									<div class="card">
+										<div class="card-header" role="tab" id="collapse11_header">
+											<h5>
+												<a class="collapsed" data-toggle="collapse" href="#collapse11" aria-expanded="false" aria-controls="collapse11">
+													What industries do you specialize in?
+												</a>
+											</h5>
+										</div>
+										<div id="collapse11" class="collapse" role="tabpanel" aria-labelledby="collapse11_header" data-parent="#accordion02">
+											<div class="card-body">
+												Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+											</div>
+										</div>
+									</div>
+
+									<div class="card">
+										<div class="card-header" role="tab" id="collapse12_header">
+											<h5>
+												<a class="collapsed" data-toggle="collapse" href="#collapse12" aria-expanded="false" aria-controls="collapse12">
+													Can you guarantee that our plan will raise capital?
+												</a>
+											</h5>
+										</div>
+										<div id="collapse12" class="collapse" role="tabpanel" aria-labelledby="collapse12_header" data-parent="#accordion02">
+											<div class="card-body">
+												Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable sunt aliqua put a bird on it squid single-origin coffee shoreditch et.
+											</div>
+										</div>
+									</div>
+
+									<div class="card">
+										<div class="card-header" role="tab" id="collapse13_header">
+											<h5>
+												<a class="collapsed" data-toggle="collapse" href="#collapse13" aria-expanded="false" aria-controls="collapse13">
+													What industries do you specialize in?
+												</a>
+											</h5>
+										</div>
+										<div id="collapse13" class="collapse" role="tabpanel" aria-labelledby="collapse13_header" data-parent="#accordion02">
+											<div class="card-body">
+												Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+											</div>
+										</div>
+									</div>
+
+								</div>
+							</div>
+
+						</div>
+
+						
+
+						
+
+						<div class="ds col-lg-6 py-lg-0 py-md-90 py-60 faq-contact column-overlay d-flex align-items-center">
+							<div class="content-center">
+								<h4 class="special-heading text-center text-capitalize">Contact us</h4>
+								<p class="special-heading text-center">Have any Questions?Ask Us!</p>
+								<div class="divider-50 hidden-below-md"></div>
+								<div class="divider-35 hidden-above-md"></div>
+
+								<div class="container">
+									<!-- <h2>Contact Us</h2> -->
+									<form id="contactForm" action="send_email.php" method="POST" novalidate>
+										<div class="form-group">
+											<label for="name">Name:</label>
+											<input type="text" id="name" name="name" required>
+											<div class="error-message" id="nameError"></div>
+										</div>
+										<div class="form-group">
+											<label for="email">Email:</label>
+											<input type="email" id="email" name="email" required>
+											<div class="error-message" id="emailError"></div>
+										</div>
+										<div class="form-group">
+											<label for="message">Message:</label>
+											<textarea id="message" name="message" required></textarea>
+											<div class="error-message" id="messageError"></div>
+										</div>
+										<button type="submit">Send Message</button>
+									</form>
+									<div id="formResponse"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+                <br><br>
+    <footer class="page_footer text-center text-sm-left  ds ms s-pt-60 s-pb-60 s-pt-md-85 s-pb-md-90 s-pt-lg-125 s-pb-lg-130 s-pt-xl-150 s-pb-xl-145 c-gutter-30 s-parallax c-mb-50 c-mb-lg-0">
+				<div class="container">
+					<div class="row justify-content-center">
+
+						<div class="col-lg-4 col-md-6  animate" data-animation="fadeInUp">
+							<a href="./" class="logo">
+								<img src="../images/kwlogo.png" alt="">
+								<span class="d-flex flex-column">
+									<span class="logo-text color-darkgrey">KW Industries</span>
+									<span class="logo-subtext">PANAGOLISH</span>
+								</span>
+							</a>
+							<p>
+							True cinnamon, or Ceylon cinnamon, is Sri Lanka's fragrant gift to the world—delicate, sweet, and prized above all others.
+							</p>
+						</div>
+
+						<div class="col-lg-2 col-md-6 animate" data-animation="fadeInUp">
+
+							<div class="widget widget_nav_menu mb-0">
+								<h3 class="widget-title">company</h3>
+								<ul class="list-unstyled">
+									<li>
+										<a href="../index.php">Home</a>
+									</li>
+									<li>
+										<a href="#">About Us</a>
+									</li>
+									<li>
+										<a href="#">Products</a>
+									</li>
+									<li>
+										<a href="#">Shop</a>
+									</li>
+									<li>
+										<a href="#">Contact Us</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						
+						<div class="col-lg-3 col-md-6 mb-0 animate" data-animation="fadeInUp">
+
+							<div class="widget widget_icons_list">
+								<h3 class="widget-title">Contact info</h3>
+								<ul>
+									<li class="icon-inline">
+										<div class="icon-styled icon-top color-main fs-14">
+											<i class="fa fa-map-marker"></i>
+										</div>
+										<p>No. 33/4 Mudduwa Road <br> Rathnapura , Sri Lanka</p>
+									</li>
+									<li class="icon-inline">
+										<div class="icon-styled icon-top color-main fs-14">
+											<i class="fa fa-phone"></i>
+										</div>
+										<p>+94 70 520 7900</p>
+									</li>
+									<li class="icon-inline">
+										<div class="icon-styled icon-top color-main fs-14">
+											<i class="fa fa-envelope"></i>
+										</div>
+										<p>
+											<a href="#">industrieskw@gmail.com</a>
+										</p>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</footer>
+
+
+			<section class="page_copyright ds ms s-py-15 s-bordertop">
+				<div class="container">
+					<div class="row align-items-center">
+
+						<div class="col-md-12 text-center">
+							<p>&copy; Copyright <span class="copyright_year">2025</span> All Rights Reserved</p>
+						</div>
+
+					</div>
+				</div>
+			</section>
+	
+
+	<script src="../js/compressed.js"></script>
+	<script src="../js/main.js"></script>
+	<script>
+  async function loadComponent(id, url) {
+    const response = await fetch(url);
+    const content = await response.text();
+    document.getElementById(id).innerHTML = content;
+  }
+
+  loadComponent('header', '../header.php');
+  loadComponent('footer', '../footer.php');
+</script>
+
+</body>
+
+</html>
